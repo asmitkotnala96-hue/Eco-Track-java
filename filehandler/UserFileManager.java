e1b5d28a231a45a2658ef2c7aca704c6ff5a60f5
Asmit <asmitkotnala96@gmail.com>
2026-03-31 23:07:15 +0530

    Add User File Manager for loading user data
    Implement UserFileManager to load user data from a file.

---
+++ filehandler/UserFileManager.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class UserFileManager {
+
+    private static final String FILE = "data/users.txt";
+
+    public static List<User> load() throws IOException {
+        List<User> list = new ArrayList<>();
+        File f = new File(FILE);
+        if (!f.exists()) return list;
+
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            list.add(User.fromFileString(line));
+        }
+        br.close();
+        return list;
+    }
+}
