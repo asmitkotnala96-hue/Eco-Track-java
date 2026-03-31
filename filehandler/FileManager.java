090420099b758fca72fea08279601e63ded3cd2a
Asmit <asmitkotnala96@gmail.com>
2026-03-31 23:06:23 +0530

    Implement File Manager for loading and saving complaints

---
+++ filehandler/FileManager.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class FileManager {
+
+    private static final String FILE = "data/complaints.txt";
+
+    public static List<Complaint> load() throws IOException {
+        List<Complaint> list = new ArrayList<>();
+        File f = new File(FILE);
+        if (!f.exists()) return list;
+
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            list.add(Complaint.fromFileString(line));
+        }
+        br.close();
+        return list;
+    }
+
+    public static void save(Complaint c) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(FILE, true));
+        bw.write(c.toFileString());
+        bw.newLine();
+        bw.close();
+    }
+
+    public static void overwrite(List<Complaint> list) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(FILE));
+        for (Complaint c : list) {
+            bw.write(c.toFileString());
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
