7fb593cf5ac6e74e705f47c723dfa5e0206b617f
Asmit <asmitkotnala96@gmail.com>
2026-04-01 21:54:20 +0530

    Update FileManager.java

--- filehandler/FileManager.java
+++ filehandler/FileManager.java
@@ -39,42 +39,50 @@ public class FileManager {
         bw.close();
     }
     public static void saveAsSeparateFile(Complaint c) throws IOException {
+        File dir = new File("data");
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
 
-    String fileName = "data/complaint_" + c.getId() + ".txt";
-
-    BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-
-    bw.write("===== Complaint Details =====");
-    bw.newLine();
-    bw.write("ID: " + c.getId());
-    bw.newLine();
-    bw.write("Description: " + c.getDescription());
-    bw.newLine();
-    bw.write("Location: " + c.getLocation());
-    bw.newLine();
-    bw.write("Status: " + c.getStatus());
-    bw.newLine();
-
-    bw.close();
+        String fileName = "data/complaint_" + c.getId() + ".txt";
+    
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+    
+        bw.write("===== Complaint Details =====");
+        bw.newLine();
+        bw.write("ID: " + c.getId());
+        bw.newLine();
+        bw.write("Description: " + c.getDescription());
+        bw.newLine();
+        bw.write("Location: " + c.getLocation());
+        bw.newLine();
+        bw.write("Status: " + c.getStatus());
+        bw.newLine();
+    
+        bw.close();
     }
 
     public static void updateSeparateFile(Complaint c) throws IOException {
+        File dir = new File("data");
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
 
-    String fileName = "data/complaint_" + c.getId() + ".txt";
+        String fileName = "data/complaint_" + c.getId() + ".txt";
+    
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+    
+        bw.write("===== Complaint Details =====");
+        bw.newLine();
+        bw.write("ID: " + c.getId());
+        bw.newLine();
+        bw.write("Description: " + c.getDescription());
+        bw.newLine();
+        bw.write("Location: " + c.getLocation());
+        bw.newLine();
+        bw.write("Status: " + c.getStatus());
+        bw.newLine();
 
-    BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-
-    bw.write("===== Complaint Details =====");
-    bw.newLine();
-    bw.write("ID: " + c.getId());
-    bw.newLine();
-    bw.write("Description: " + c.getDescription());
-    bw.newLine();
-    bw.write("Location: " + c.getLocation());
-    bw.newLine();
-    bw.write("Status: " + c.getStatus());
-    bw.newLine();
-
-    bw.close();
+        bw.close();
     }
 }
